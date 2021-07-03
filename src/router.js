d532076e699d8bb7714950c317fcde85defecfe3
Mohammad Za'areer <zaa92reer@gmail.com>
2021-07-03 21:57:54 +0300

    added some routes

--- src/router.js
+++ src/router.js
@@ -17,6 +17,19 @@ router.post('/categories', async(req, res) => {
     res.status(201).json(result);
 });
 
+router.put('/categories/:id', async(req, res) => {
+    const id = req.params.id;
+    const result = await Categories.findByIdAndUpdate(id, req.body, {
+       new: true
+   });
+    res.status(200).json(result);
+});
+router.delete('/categories/:id', async(req, res) => {
+    const id = req.params.id;
+    const result = await Categories.findByIdAndDelete(id);
+    res.status(200).json(result);
+});
+
 router.get('/products', async(req, res) => {
     const allProducts = await Products.find({});
     res.status(200).json(allProducts);
@@ -26,6 +39,19 @@ router.post('/products', async(req, res) => {
     const product = new Products(req.body);
     const result = await product.save(req.body);
     res.status(201).json(result);
+});
+
+router.put('/products/:id', async(req, res) => {
+    const id = req.params.id;
+    const result = await Products.findByIdAndUpdate(id, req.body, {
+       new: true
+   });
+    res.status(200).json(result);
+});
+router.delete('/products/:id', async(req, res) => {
+    const id = req.params.id;
+    const result = await Products.findByIdAndDelete(id);
+    res.status(200).json(result);
 });
 
 module.exports = router;
